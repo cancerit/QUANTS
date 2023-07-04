a95d05ff090c5dab3023d0b04b2dd6697dab2273
Ian Vermes <iv3@sanger.ac.uk>
2023-07-04 17:58:48 +0000

    Tighten up validation logic for output file

--- bin/manifest_transformer/src/args/validate.py
+++ bin/manifest_transformer/src/args/validate.py
@@ -20,9 +20,31 @@ def assert_valid_input_file(input_file: "Path") -> "Path":
 
 
 def assert_valid_output_file(output_file: "Path") -> "Path":
-    if output_file.parent.exists():
-        check_write_permissions(output_file.parent)
-    else:
-        msg = f"Output file {output_file!r} directory does not exist."
+    """
+    Output file should correspond to a file but it does not have to exist yet.
+
+    If it does exist, it's parent directory must exist and be writable.
+    """
+    exists = output_file.exists()
+    is_dir = output_file.is_dir()  # If a dir does not exist, this will be False
+    parent_exists = output_file.parent.exists()
+
+    forbidden_conditions = any(
+        [
+            is_dir,
+            not exists and not parent_exists,
+        ]
+    )
+    if forbidden_conditions:
+        msg = f"Output file {output_file!r} must be a file and its parent directory must exist."
         raise exc.ValidationError(msg)
+
+    # We know that output_file is not a directory and its parent directory
+    # exists, but it may not exist itself. If it does exist, it must be a file
+    # that will be over-written. If it does not exist, it WILL be a file.
+    #
+    # Hence we need to check that the parent directory is writable.
+
+    validation_candiate = output_file.parent
+    check_write_permissions(validation_candiate)
     return output_file
