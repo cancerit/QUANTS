d574ddc1ba53c3987116c8cafa2d96b5618bb55b
Ian Vermes <iv3@sanger.ac.uk>
2023-07-06 14:44:12 +0000

    Add an a way of cleaning the summary file

--- bin/manifest_transformer/src/args/clean.py
+++ bin/manifest_transformer/src/args/clean.py
@@ -1,4 +1,5 @@
 import typing as t
+import datetime
 
 if t.TYPE_CHECKING:
     from pathlib import Path
@@ -48,3 +49,46 @@ class OutputFile:
         clean_input_file = self._input_file.clean
         output_file = finalise_output_file(clean_input_file, self._output_file)
         return validate.assert_valid_output_file(output_file)
+
+
+class SummaryFile:
+    """
+    Validates a summary file, inferring it from the input file if necessary.
+
+    If the summary file is inferred from the input file, it has the following
+    format: '<input_file>.summary.<date>.json'
+
+    A clean summary file is a file that may not exist but whose parent directory
+    is writable.
+
+    Special conditions:
+    - If the summary file is None, the input file is returned as the clean summary file.
+    - If the summary file is an existing file, it is returned as the clean summary file.
+    - If the summary file is a non-existant file, it is returned as the clean summary file.
+    - If the summary file is an existing directory, that directory and input
+        filename are concatenated as clean summary file.
+    """
+
+    def __init__(self, input_file: "Path", summary_file: t.Optional["Path"]):
+        self._input_file: "InputFile" = InputFile(input_file)
+        self._summary_file: t.Optional["Path"] = summary_file
+
+    def _summary_name(self, input_path: "Path") -> str:
+        now = datetime.datetime.now()
+        # Format the date as 2022-12-11T10-09-08
+        date_str = now.strftime("%Y-%m-%dT%H-%M-%S")
+        new_suffix = ".json"
+        new_midfix = f".summary.{date_str}"
+        new_name = input_path.stem + new_midfix + new_suffix
+        return new_name
+
+    @property
+    def clean(self) -> "Path":
+        clean_input_file = self._input_file.clean
+        # The inferred file is the input file but with the name and suffix
+        # changed. This is not going to mean the summary file will use the
+        # inferred file, but it might depending on how finalise_output_file
+        # evaluates the output file.
+        inferred_file = clean_input_file.with_name(self._summary_name(clean_input_file))
+        summary_file = finalise_output_file(inferred_file, self._summary_file)
+        return validate.assert_valid_output_file(summary_file)
--- bin/manifest_transformer/tests/unit/args/test_clean.py
+++ bin/manifest_transformer/tests/unit/args/test_clean.py
@@ -1,11 +1,26 @@
-from unittest import expectedFailure
 import pytest
 from shutil import rmtree
+import datetime
+from pathlib import Path
 
 from src.args import clean
 from src import exceptions as exc
 
+# CONSTANTS
+ARBITRARY_DATETIME = datetime.datetime(2022, 12, 11, 10, 9, 8)
+FORMATTED_ARBITRARY_DATETIME = "2022-12-11T10-09-08"
+
 # FIXTURES
+
+
+@pytest.fixture
+def fixed_datetime(monkeypatch):
+    class MyDateTime:
+        @classmethod
+        def now(cls):
+            return ARBITRARY_DATETIME
+
+    monkeypatch.setattr(datetime, "datetime", MyDateTime)
 
 
 @pytest.fixture()
@@ -95,6 +110,68 @@ def test_OutputFile__without_a_path_specified(input_file_setup):
 
     # When
     actual = clean.OutputFile(input_file, None).clean
+
+    # Then
+    assert actual == expected
+
+
+def test_SummaryFile__with_a_file_path(
+    input_file_setup, example_dir_setup, fixed_datetime
+):
+    # Given
+    input_file = input_file_setup
+    summary_file = example_dir_setup / "summary_file.json"
+    expected_name = "summary_file.json"
+    expected = example_dir_setup / expected_name
+
+    # When
+    actual = clean.SummaryFile(input_file, summary_file).clean
+
+    # Then
+    assert actual == expected
+
+
+def test_SummaryFile__with_a_preexisting_file_path(
+    input_file_setup, example_dir_setup, fixed_datetime
+):
+    # Given
+    input_file = input_file_setup
+    summary_file = example_dir_setup / "summary_file.json"
+    summary_file.touch()
+    expected_name = "summary_file.json"
+    expected = example_dir_setup / expected_name
+
+    # When
+    actual = clean.SummaryFile(input_file, summary_file).clean
+
+    # Then
+    assert actual == expected
+
+
+def test_SummaryFile__with_a_dir_path(
+    input_file_setup, example_dir_setup, fixed_datetime
+):
+    # Given
+    input_file = input_file_setup
+    summary_dir = example_dir_setup
+    expected_name = f"{input_file.stem}.summary.{FORMATTED_ARBITRARY_DATETIME}.json"
+    expected = summary_dir / expected_name
+
+    # When
+    actual = clean.SummaryFile(input_file, summary_dir).clean
+
+    # Then
+    assert actual == expected
+
+
+def test_SummaryFile__without_a_path_specified(input_file_setup, fixed_datetime):
+    # Given
+    input_file = input_file_setup
+    expected_name = f"{input_file.stem}.summary.{FORMATTED_ARBITRARY_DATETIME}.json"
+    expected = input_file.parent / expected_name
+
+    # When
+    actual = clean.SummaryFile(input_file, None).clean
 
     # Then
     assert actual == expected
